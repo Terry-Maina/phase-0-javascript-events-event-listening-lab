6f0e1ef8996299681ea3afef0b2cf4b49508760b
Teresa <tenjerim@gmail.com>
2022-12-04 18:13:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
 }
+function addingEventListener(click){
+
+  const input = document.getElementById('input');
+  input.addEventListener('click', function() {
+      alert('I was clicked!');
+    })
+}
